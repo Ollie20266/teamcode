bd551f67c86be27359a2995d122250b3b08cb4bc
Ollie20266 <ollie@ohagan.me>
2025-03-10 19:51:27 -0700

    Added deposit for spec 2 and spec 3

--- src/main/java/org/firstinspires/ftc/teamcode/SpecAuto.java
+++ src/main/java/org/firstinspires/ftc/teamcode/SpecAuto.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.SleepAction;
@@ -20,10 +19,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 //@Disabled
 public class SpecAuto extends LinearOpMode {
 
-    private final int ARM_LIFT_DOWN = 0;
+
     private final int ARM_LIFT_UP = 2850;
     private final int ARM_EXTEND_RETRACTED = 1;
-    private final int ARM_EXTEND_HORIZ = 435;
     private final int ARM_EXTEND_VERT_SPEC_PRE_HANG = 285;
     private final int ARM_EXTEND_VERT_SPEC_POST_HANG = 450;
     private final double CLAW_PITCH_DOWN = 0.85;
@@ -32,9 +30,6 @@ public class SpecAuto extends LinearOpMode {
     private final double CLAW_YAW_NEUTRAL = 0.48;
     private final double CLAW_OPEN = 0.49;
     private final double CLAW_CLOSED = 0.11;
-    private final double CLAW_DUMP_TIME = 0.5;
-    private final double CLAW_CLOSE_TIME = 0.5;
-    private final double CLAW_OPEN_TIME = 0.5;
 
     @Override
     public void runOpMode() {
@@ -66,7 +61,7 @@ public class SpecAuto extends LinearOpMode {
         waitForStart();
 
         Actions.runBlocking(new SequentialAction(
-                //Bring spec 1 to the bar
+                //Deposit spec 1
                 new SequentialAction(
                         new SleepAction(0.1),
                         new MotorAction(armLift, ARM_LIFT_UP, 10),
@@ -75,11 +70,10 @@ public class SpecAuto extends LinearOpMode {
                                 .strafeToLinearHeading(new Vector2d(5,-30), Math.toRadians(270.0))
                                 .build()
                 ),
-                //Deposit spec 1
                 new MotorAction(armExtend, ARM_EXTEND_VERT_SPEC_POST_HANG, 10),
                 new ServoAction(claw, CLAW_OPEN, 0.5),
                 new MotorAction(armExtend, ARM_EXTEND_RETRACTED, 40),
-                new ServoAction(clawPitch, CLAW_PITCH_NEUTRAL, 0.5),
+                new ServoAction(clawPitch, CLAW_PITCH_DOWN, 0.5),
 
                 //Drive to spec 2
                 drive.actionBuilder(new Pose2d(5,-34,Math.toRadians(270.0)))
@@ -91,10 +85,12 @@ public class SpecAuto extends LinearOpMode {
                 drive.actionBuilder(new Pose2d(33,-14,Math.toRadians(270.0)))
                         .strafeToLinearHeading(new Vector2d(44, -14), Math.toRadians(270.0))
                         .build(),
+
                 //Push spec 2 to human player
                 drive.actionBuilder(new Pose2d(44,-14,Math.toRadians(270.0)))
                         .strafeToLinearHeading(new Vector2d(46, -52), Math.toRadians(270.0))
                         .build(),
+
                 //Drive to spec 3
                 drive.actionBuilder(new Pose2d(46,-52,Math.toRadians(270.0)))
                         .strafeToLinearHeading(new Vector2d(46, -14), Math.toRadians(270.0))
@@ -102,11 +98,44 @@ public class SpecAuto extends LinearOpMode {
                 drive.actionBuilder(new Pose2d(46,-14,Math.toRadians(270.0)))
                         .strafeToLinearHeading(new Vector2d(54, -14), Math.toRadians(270.0))
                         .build(),
+
                 //Push spec 3 to human player
                 drive.actionBuilder(new Pose2d(54,-14,Math.toRadians(270.0)))
                         .strafeToLinearHeading(new Vector2d(52, -52), Math.toRadians(270.0))
-                        .build()
-                //Need to add other spec
+                        .build(),
+
+                //Deposit spec 2
+                drive.actionBuilder(new Pose2d(52,-52,Math.toRadians(270.0)))
+                        .strafeToLinearHeading(new Vector2d(40, -58), Math.toRadians(270.0))
+                        .build(),
+                new ServoAction(clawPitch, CLAW_PITCH_NEUTRAL, 0.5),
+                new ServoAction(claw, CLAW_CLOSED, 0.5),
+                new MotorAction(armLift, ARM_LIFT_UP, 10),
+                new ServoAction(clawPitch, CLAW_PITCH_UP, 0.5),
+                new MotorAction(armExtend, ARM_EXTEND_VERT_SPEC_PRE_HANG, 10),
+                drive.actionBuilder(new Pose2d(40, -58,Math.toRadians(270.0)))
+                        .strafeToLinearHeading(new Vector2d(7,-30), Math.toRadians(270.0))
+                        .build(),
+                new MotorAction(armExtend, ARM_EXTEND_VERT_SPEC_POST_HANG, 10),
+                new ServoAction(claw, CLAW_OPEN, 0.5),
+                new MotorAction(armExtend, ARM_EXTEND_RETRACTED, 40),
+                new ServoAction(clawPitch, CLAW_PITCH_NEUTRAL, 0.5),
+
+                //Deposit spec 3
+                drive.actionBuilder(new Pose2d(7, -30,Math.toRadians(270.0)))
+                        .strafeToLinearHeading(new Vector2d(40, -58), Math.toRadians(270.0))
+                        .build(),
+                new ServoAction(claw, CLAW_CLOSED, 0.5),
+                new MotorAction(armLift, ARM_LIFT_UP, 10),
+                new ServoAction(clawPitch, CLAW_PITCH_UP, 0.5),
+                new MotorAction(armExtend, ARM_EXTEND_VERT_SPEC_PRE_HANG, 10),
+                drive.actionBuilder(new Pose2d(40, -58,Math.toRadians(270.0)))
+                        .strafeToLinearHeading(new Vector2d(9,-30), Math.toRadians(270.0))
+                        .build(),
+                new MotorAction(armExtend, ARM_EXTEND_VERT_SPEC_POST_HANG, 10),
+                new ServoAction(claw, CLAW_OPEN, 0.5),
+                new MotorAction(armExtend, ARM_EXTEND_RETRACTED, 40),
+                new ServoAction(clawPitch, CLAW_PITCH_NEUTRAL, 0.5)
         ));
     }
 
